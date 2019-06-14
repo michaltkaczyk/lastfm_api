f266de68a28e5c420a17a5f1159937eff60d420f
michaltkaczyk <michaltkaczyk@outlook.com>
2019-06-14 20:54:15 +0200

    First 50 Tracks
    Add a simple loop to extract first 50 scrobbles

--- main.py
+++ main.py
@@ -3,9 +3,30 @@ import requests
 user = open("credentials/lastfm_user.txt").read()
 api_key = open("credentials/lastfm_key.txt").read()
 
-path = "http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=" + user + "&api_key=" + api_key + \
-       "&format=json"
+LFM_API_ROOT = "http://ws.audioscrobbler.com/2.0/"
 
-response = requests.get(path)
+lfm_api_path = LFM_API_ROOT +\
+               "?method=user.getrecenttracks&user=" + user +\
+               "&api_key=" + api_key +\
+               "&format=json"
+
+response = requests.get(lfm_api_path)
 print(response.status_code)
 
+results = response.json()['recenttracks']['track']
+
+tracks_list = []
+
+for scrobbled_item in results:
+
+    # Skip currently playing track
+    if '@attr' in scrobbled_item:
+        if 'nowplaying' in scrobbled_item['@attr']:
+            if scrobbled_item['@attr']['nowplaying'] == "true":
+                continue
+
+    tracks_list.append(
+        [scrobbled_item['artist']['#text'],
+         scrobbled_item['album']['#text'],
+         scrobbled_item['name'],
+         scrobbled_item['date']['#text']])
